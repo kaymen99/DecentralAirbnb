22f9b05f75f374df687e9687605c495fa5eb974e
Aymen <AymenMir1001@gmail.com>
2022-11-26 23:04:47 +0100

    Delete src/pages directory

--- src/pages/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Home } from './Home';
-export { default as Dashboard } from './Dashboard';
-export { default as Rentals } from './Rentals';
-export { default as AddRental } from './AddRental';
