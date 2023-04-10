5ddcd46814412d2e9043490316b389484dbc4c50
Aymen <AymenMir1001@gmail.com>
2023-04-10 12:21:28 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom/client";
 import './index.css';
 import { combineReducers } from 'redux'
 import reportWebVitals from './reportWebVitals';
@@ -46,15 +46,15 @@ const store = configureStore({
 
 let persistor = persistStore(store)
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <App />
       </PersistGate>
     </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 reportWebVitals();
